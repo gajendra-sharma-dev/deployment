a94bdc3dba1fb1e74e353f580443b5b91285ae24
gajendra sharma <satishsharma9649881554@gmail.com>
2026-09-07 14:38:10 +0530

    add new files

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const port = 3000
 
 
 app.use(cors({
-  origin: "https://deployment-gajendra8.vercel.app"
+  origin: "https://deployment-black-three.vercel.app"
 }))
 
 app.get('/api/code', (req, res) => {
